3970056d3fae5653f2568453d05fc691b650b541
martinsvadlenka <martin.svadlenka@cloudfield.cz>
2024-02-16 12:03:01 +0100

    send transaction and validations added v0.9

--- client/send_transaction_post.py
+++ client/send_transaction_post.py
@@ -4,7 +4,6 @@ import requests
 
 url = "http://localhost:10003/api/transaction_post"
 
-
 headers = {
     "Content-Type": "application/json",  # Replace with the appropriate content type for your image file
 }
@@ -12,17 +11,17 @@ headers = {
 json_data = {
     "sell_id": "123456",
     "device_id": "abc123",
-    "time": "202402091200",
+    "time": "202402091200", # format yyyyMMddhhmm
     "neural_version": "v1.0",
-    "success": True,
+    "success": True,  # udava zda byla vybrana polozka z nabidky OR, nebo jina
     "sold_assortment": "apple_jonaprice",
     "success_position": 1,
     "position_1_percent": 25.5,
     "position_2_percent": 5.0,
     "position_3_percent": 07.25,
     "position_4_percent": 0.99,
-    "position_5_percent": 0.25,
-    "position_1_assortment": "apple_jonaprice",
+    "position_5_percent": 0.25,  # validace na soucet procent not bigger than 1
+    "position_1_assortment": "Apple_jonaprice",
     "position_2_assortment": "tomato_organic",
     "position_3_assortment": "peach_organic",
     "position_4_assortment": "pear",
